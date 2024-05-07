065056ce3f8b832feec39e1c554355a858158865
riodamanik98 <rio71385@gmail.com>
2024-05-07 21:57:58 +0700

    initial configuration

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 function validation() {
     console.log('good');
+    alert("GOOD");
 }
 
 document.getElementById('home-btn').addEventListener('click' , () => validation ())
+document.getElementById('home-btn2').addEventListener('click' , () => validation ())
+document.getElementById('home-btn3').addEventListener('click' , () => validation ())
+
