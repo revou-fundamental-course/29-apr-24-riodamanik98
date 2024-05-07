6b98361413c25a7984e297f08ceceb57840927e3
riodamanik98 <rio71385@gmail.com>
2024-05-07 23:16:49 +0700

    initial configuration

--- js/script.js
+++ js/script.js
@@ -9,3 +9,7 @@ document.getElementById('home-btn').addEventListener('click' , () => validation 
 document.getElementById('home-btn2').addEventListener('click' , () => validation ())
 document.getElementById('home-btn3').addEventListener('click' , () => validation ())
 
+function ubahNama() {
+    var namaElement = document.getElementById("nama");
+    namaElement.textContent = "By Rio Damanik"; // Mengubah teks menjadi "Hei"
+}
