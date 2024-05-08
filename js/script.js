cdab4afb496f7157992f032cc106fdbb98af3a3e
riodamanik98 <rio71385@gmail.com>
2024-05-08 11:54:41 +0700

    initial configuration

--- js/script.js
+++ js/script.js
@@ -13,3 +13,43 @@ function ubahNama() {
     var namaElement = document.getElementById("nama");
     namaElement.textContent = "By Rio Damanik"; // Mengubah teks menjadi "Hei"
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("myForm");
+    const usernameInput = document.getElementById("username");
+    const emailInput = document.getElementById("email");
+    const optionInput = document.getElementById("option");
+    const usernameError = document.getElementById("username-error");
+    const emailError = document.getElementById("email-error");
+    const optionError = document.getElementById("option-error");
+  
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const username = usernameInput.value;
+      const email = emailInput.value;
+      const option = optionInput.value;
+  
+      if (username === "") {
+        usernameError.innerHTML = "Username Tidak boleh kosong!";
+      } else {
+        usernameError.innerHTML = "";
+      }
+  
+      if (email === "") {
+        emailError.innerHTML = "Email Tidak boleh kosong!";
+      } else {
+        emailError.innerHTML = "";
+      }
+  
+      if (option === "") {
+        optionError.innerHTML = "Option Tidak boleh kosong!";
+      } else {
+        optionError.innerHTML = "";
+      }
+  
+      if (username!== "" && email!== "" && option!== "") {
+        // Form is valid, you can submit it here
+        console.log("Form is valid!");
+      }
+    });
+  });
